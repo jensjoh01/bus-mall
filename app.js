3e34b64c4c1848bc5f5acc23370f17c2f24c57d3
jensjoh01 <johnnyj2439@gmail.com>
2017-07-26 08:51:47 -0700

    commit wednesday morning

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 Image.previousNumber = [];
+Image.votes = [];
 
 function Image(name) {
   this.name = name;
@@ -59,7 +60,6 @@ function displayImages() {
   Image.all[number[0]].timesShown += 1;
   Image.all[number[1]].timesShown += 1;
   Image.all[number[2]].timesShown += 1;
-
   Image.previousNumber = number;
 }
 
@@ -74,20 +74,103 @@ function clickHandler(e) {
   for(var i = 0; i < Image.all.length; i++){
     if(e.target.alt === Image.altNames[i]){
       Image.all[i].timesClicked += 1;
+      Image.votes[i] = Image.all[i].timesClicked;
     }
   }
-  if(Image.clickCount < 3){
+  if(Image.clickCount < 25){
     displayImages();
   } else {
-    for(var j = 0; j < Image.all.length; j++){
-      var listEl = document.getElementById('list');
-      var liEl = document.createElement('li');
-      liEl.textContent = Image.all[j].timesClicked + ' for the ' + Image.altNames[j];
-      listEl.appendChild(liEl);
-    }
     document.getElementById('container').removeEventListener('click', clickHandler);
+  }
+}
+
+// function randomColors() {
+//   var color = Math.floor(Math.random() * 200);
+//   return color;
+// }
+
+function drawChart(){
+  // document.getElementById('myChart').innerHTML = ;
+  var ctx = document.getElementById('myChart').getContext("2d");
+  var myChart = new Chart (ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: Image.altNames,
+      datasets: [{
+        label: '# of votes',
+        data: Image.votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: false,
+      scales: {
+        xAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 0.5
+          }
+        }]
+      }}
+  });
+};
+
+function showList() {
+  for(var i = 0; i < Image.all.length; i++){
+    var listEl = document.getElementById('list');
+    var liEl = document.createElement('li');
+    liEl.textContent = Image.all[i].timesClicked + ' for the ' + Image.altNames[i];
+    listEl.appendChild(liEl);
   }
 }
 
 document.getElementById('container').addEventListener('click', clickHandler);
 displayImages();
+
+document.getElementById('draw-chart').addEventListener('click', drawChart);
+
+document.getElementById('display-list').addEventListener('click', showList);
